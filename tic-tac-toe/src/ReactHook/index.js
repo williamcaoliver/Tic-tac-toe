87d6aa69789b6d5a248b9c11095de69df4ec045b
William Oliver <woliver@scottlogic.com>
2022-12-08 14:00:51 +0000

    Played with CSS

--- tic-tac-toe/src/ReactHook/index.js
+++ tic-tac-toe/src/ReactHook/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/* import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
@@ -117,3 +117,4 @@ root.render(
   
     <Game />
 );
+ */
